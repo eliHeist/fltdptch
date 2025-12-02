8bb32270a8447efeee35c71e6c0075fe5eb39232
eliHeist <eliyang256@outlook.com>
2025-12-02 09:15:23 +0300

    first commit

---
+++ fltdptch/appsConfig.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+
+app_configs = [
+	{ 'app_name': 'accounts', 'url': 'accounts/', 'namespace': 'accounts' },
+
+    # { "app_name": "finances.payments", "url": "finances/payments", "namespace": "payments" },
+]
+
+def getAppUrls():
+    urlpatterns = []
+    for config in app_configs:
+        urlpatterns.append(
+            path(f"{config['url']}", include(f"{config['app_name']}.urls", namespace=config['namespace']))
+        )
+    return urlpatterns
+
+def getAppNames():
+    return [config['app_name'] for config in app_configs]
