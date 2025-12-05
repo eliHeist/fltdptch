257ec97d718f9efb57d7f0819d90873eeb3133eb
eliHeist <eliyang256@outlook.com>
2025-12-05 18:33:32 +0300

    create superuser profile

--- accounts/models.py
+++ accounts/models.py
@@ -51,6 +51,8 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
+        UserProfile.objects.create(user=user)
         return user
 
     def create_superuser(self, email, password):
